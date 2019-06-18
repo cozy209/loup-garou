5ec8d95a6b684309d67fa30e17451873524c17cc
nini <virginie.sansone@mines-ales.org>
2019-06-18 23:33:35 +0200

    Client et server socket ajoutés. Communication entre les deux.

--- src/main/java/org/emacv/loupgarou/Client.java
+++ src/main/java/org/emacv/loupgarou/Client.java
@@ -1,24 +1,133 @@
 package org.emacv.loupgarou;
 
-import org.emacv.loupgarou.personnages.Personnage;
+import java.net.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class Client {
+    private Socket           socket = null;
+    private DataInputStream  input  = null;
+    private DataInputStream  in  = null;
+    private DataOutputStream out    = null;
 
-    private Integer id;
-    private String pseudo;
-    private Personnage personnage;
+    private String       carte  = "loup";
+    private String       joueur = "toto";
+    private List<String> persos = new ArrayList<String>();
 
-    public Client(Integer id, String pseudo) {
-        this.id = id;
-        this.pseudo = pseudo;
+    public Client(String address, int port) {
+
+        persos.add("toto");
+        persos.add("titi");
+        persos.add("tata");
+
+        try {
+            socket = new Socket(address, port);
+            System.out.println("Connected");
+
+            // takes input from terminal
+            input = new DataInputStream(System.in);
+
+            //
+            in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+
+            // sends output to the socket
+            out = new DataOutputStream(socket.getOutputStream());
+
+        } catch (UnknownHostException u) {
+            System.out.println(u);
+        } catch (IOException i) {
+            System.out.println(i);
+        }
+
+        // string to read message from input
+        String lineInput = "";
+        String line = "";
+
+        // keep reading until "Over" is input
+        while (!lineInput.equals("Quit") || !line.contains("end")) {
+            try {
+                lineInput = input.readLine();
+                out.writeUTF(lineInput);
+
+                line = in.readUTF();
+
+                tour(line,lineInput);
+
+            } catch (IOException i) {
+                System.out.println(i);
+            }
+        }
+
+        try {
+            input.close();
+            out.close();
+            socket.close();
+        } catch (IOException i) {
+            System.out.println(i);
+        }
     }
 
-    @Override
-    public String toString() {
-        return String.format("%s -> %s", this.pseudo, this.personnage);
+    public static void main(String args[]) {
+        Client client = new Client("127.0.0.1", 5000);
     }
 
-    public void setPersonnage(Personnage personnage){
-        this.personnage = personnage;
+    private void loup(){
+        if (carte.equals("loup")){
+            System.out.println("Entrez le nom du villageois a tuer "+persos.toString());
+        }
+    }
+    private void vote(){
+        System.out.println("Qui suspectez vous? "+persos.toString());
+    }
+
+    private void nuit(){
+        System.out.println("Mode nuit activé");
+    }
+    private void jour(){
+        System.out.println("Mode jour activé");
+    }
+    private void mort(String line){
+        String[] tab = line.split(Pattern.quote("."));
+        String mort = tab[1];
+        if (persos.contains(mort)) {
+            System.out.println(mort + " est mort, tué par les loups");
+            persos.remove(mort);
+        }
+    }
+
+    private void tour(String line,String lineInput) throws IOException{
+        line.toLowerCase();
+
+        if (line.contains("nuit")){
+            if (line.contains("mort")){
+                mort(line);
+            }
+            nuit();
+        }
+        if (line.contains("jour")){
+            jour();
+            if (line.contains("mort")){
+                mort(line);
+            }
+        }
+
+        if (line.contains("loup")){
+            loup();
+        }
+
+        if (line.contains("vote")){
+            vote();
+        }
+
+        if (persos.contains(lineInput.toLowerCase())){
+            out.writeUTF(lineInput);
+        }
+
+        if (line.contains("end")){
+            System.out.println(persos.get(0)+" a gagné");
+            System.exit(0);
+        }
     }
 }
--- src/main/java/org/emacv/loupgarou/Server.java
+++ src/main/java/org/emacv/loupgarou/Server.java
@@ -1,4 +1,105 @@
 package org.emacv.loupgarou;
 
+import java.net.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Server {
+    private Socket           socket = null;
+    private ServerSocket     server = null;
+    private DataInputStream  in     = null;
+    private DataOutputStream out    = null;
+
+    List<String> joueurs = new ArrayList<String>();
+    boolean nuit;
+    boolean loup;
+    boolean end;
+
+    public Server(int port) {
+
+        joueurs.add("toto");
+        joueurs.add("titi");
+        joueurs.add("tata");
+        end = false;
+
+        try {
+            server = new ServerSocket(port);
+            System.out.println("Server started");
+
+            System.out.println("Waiting for a client ...");
+
+            socket = server.accept();
+            System.out.println("Client accepted");
+
+            in = new DataInputStream(
+                    new BufferedInputStream(socket.getInputStream()));
+
+            out = new DataOutputStream(socket.getOutputStream());
+
+            String line = "";
+
+            while (!line.equals("Quit") || !end) {
+                try {
+                    line = in.readUTF();
+                    System.out.println(line);
+
+                    tour(line);
+
+                } catch (IOException i) {
+                    System.out.println(i);
+                }
+            }
+            System.out.println("Closing connection");
+
+            // close connection
+            socket.close();
+            in.close();
+        } catch (IOException i) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String args[]) {
+        Server server = new Server(5000);
+    }
+
+
+    private void tour(String line) throws IOException{
+        line.toLowerCase();
+
+        if (line.contains("go")){
+            nuit = true;
+            loup = true;
+            out.writeUTF("nuit\nloup");
+        }
+        if (loup && joueurs.contains(line)){
+            String stringOut = "mort."+line+".jour";
+            loup = false;
+            nuit = false;
+            joueurs.remove(line);
+            if (joueurs.size() == 1){
+                System.out.println("fin");
+                stringOut+=".end";
+                end = true;
+            } else {
+                stringOut+=".vote";
+            }
+            out.writeUTF(stringOut);
+        }
+        if (!nuit && joueurs.contains(line)){
+            String stringOut = "mort."+line+".nuit";
+            nuit = true;
+            loup = true;
+            joueurs.remove(line);
+            if (joueurs.size() == 1){
+                System.out.println("fin");
+                stringOut += ".end";
+                end = true;
+            } else {
+                stringOut+=".loup";
+            }
+            out.writeUTF(stringOut);
+        }
+    }
 }
